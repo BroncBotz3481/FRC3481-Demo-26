544fc49766c955823d0e0e56911eb4c52d056ae7
aircondition-png <aarohanchakravarty10@gmail.com>
2026-09-07 19:48:06 -0500

    Fixed swerve
    mightve broke the code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -29,21 +29,13 @@ public class RobotContainer {
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
       
-  SwerveInputStream driveAngularVelocity =
-      SwerveInputStream.of(
-                           drivebase.getSwerveDrive(),
-                           () -> m_driverController.getLeftY() * -1,
-                           () -> m_driverController.getLeftX() * -1)
-                       .withControllerRotationAxis(() -> m_driverController.getRightX() * -1)
-                       .deadband(OperatorConstants.DEADBAND)
-                       .scaleTranslation(1.0)
-                       .scaleRotation(0.8)
-                       .allianceRelativeControl(true);
 
-  SwerveInputStream stream = driveAngularVelocity.copy().withControllerHeadingAxis(
-                                                              ()->  Math.cos(Degrees.of(headingDegrees).in(Radians)), 
-                                                              ()->Math.sin(Degrees.of(headingDegrees).in(Radians)))
-                                                            .headingWhile(true);
+  private final SwerveInputStream driveAngularVelocity =
+      drivebase.getAngularVelocityStream(
+                m_driverController::getLeftY,
+                m_driverController::getLeftX,
+                () -> m_driverController.getRawAxis(2))
+            .withAllianceRelativeControl();
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
@@ -62,12 +54,14 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    drivebase.setDefaultCommand(drivebase.drive(driveAngularVelocity));
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    m_driverController.start().and(m_driverController.back()).onTrue(drivebase.zeroGyro());
   }
 
   /**
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -1,83 +1,98 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
-
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import static edu.wpi.first.units.Units.Meter;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
 
 import java.io.File;
-import edu.wpi.first.wpilibj.Filesystem;
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.pathplanner.lib.path.PathConstraints;
+
 import swervelib.parser.SwerveParser;
-import swervelib.SwerveDrive;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
-import frc.robot.Constants;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import yams.mechanisms.config.SwerveDriveConfig;
+import yams.mechanisms.swerve.SwerveDrive;
+import yams.mechanisms.swerve.utility.SwerveInputStream;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 
+public class SwerveSubsystem extends SubsystemBase
+{
+  private static final PathConstraints PATHFINDING_CONSTRAINTS = new PathConstraints(
+      MetersPerSecond.of(3.0), MetersPerSecondPerSecond.of(3.0),
+      DegreesPerSecond.of(540), DegreesPerSecondPerSecond.of(720));
 
-public class SwerveSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  File directory = new File(Filesystem.getDeployDirectory(),"swerve");
-  SwerveDrive swerveDrive;
-  public SwerveSubsystem() {
-    try
+  private SwerveDrive drive;
+  private Pigeon2 gyro;
+  private StatusSignal<AngularVelocity> gyroX;
+  private StatusSignal<AngularVelocity> gyroY;
+  private StatusSignal<AngularVelocity> gyroZ;
+
+  public SwerveSubsystem()
+  {
+    var cfg = new SwerveDriveConfig()
+        .withStartingPose(new Pose2d(3, 3, Rotation2d.kZero))
+        .withSubsystem(this)
+        .withTranslationController(new PIDController(4, 0, 0))
+        .withRotationController(new PIDController(1, 0, 0))
+        .withTelemetry(TelemetryVerbosity.HIGH);
+    try 
     {
-      swerveDrive = new SwerveParser(directory)
-          .createSwerveDrive(
-              Constants.maxSpeed,
-              new Pose2d(
-                  new Translation2d(Meter.of(13), Meter.of(4)), Rotation2d.fromDegrees(180)));
-      // Alternative method if you don't want to supply the conversion factor via JSON
-      // files.
-      // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed,
-      // angleConversionFactor, driveConversionFactor);
+      drive = new SwerveParser(new File(Filesystem.getDeployDirectory(), "swerve-config"))
+          .createSwerveDrive(cfg);
     } catch (Exception e)
     {
       throw new RuntimeException(e);
     }
   }
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+  public SwerveInputStream getAngularVelocityStream(DoubleSupplier x, DoubleSupplier y,
+                                                    DoubleSupplier rot)
+  {
+    return new SwerveInputStream(drive, x, y, rot);
   }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
+  public Command drive(SwerveInputStream stream)
+  {
+    return drive.drive(() -> ChassisSpeeds.fromFieldRelativeSpeeds(stream.get(),
+                                                                   new Rotation2d(drive.getGyroAngle())));
+  }
+
+  /** Zero the gyro heading. Bind this to a button combo for field recovery. */
+  public Command zeroGyro()
+  {
+    return runOnce(() -> drive.zeroGyro());
   }
 
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public void periodic()
+  {
+    drive.updateTelemetry();
   }
 
   @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+  public void simulationPeriodic()
+  {
+    drive.simIterate();
   }
-
+  
   public Object getSwerveDrive() {
     // TODO Auto-generated method stub
-    return swerveDrive;
-  }
+    return drive;
+ }
 }
+  
